0a979cede25f5513104cc827c5bbffc910ebe148
Sbats <simon.bats12@gmail.com>
2016-06-10 18:24:18 +0200

    Bind submitHandling to form

--- public/assets/js/geotrek.js
+++ public/assets/js/geotrek.js
@@ -3,44 +3,24 @@
 
     function getParams(form) {
 
-        if (form) {
-
-            var name = form.querySelector('input[name="fname"]').value;
-            var email = form.querySelector('input[name="email"]').value;
-            var phone = form.querySelector('input[name="phone"]').value;
-            var company = form.querySelector('input[name="company"]').value;
-            var message = form.querySelector('textarea').value;
-            message =  encodeURIComponent(
-                'Organisation: ' + company
-                + '\n'
-                + '\n'
-                + message
-            );
-
-            return (
-                'name=' + name
-                + '&email=' + email
-                + '&phone=' + phone
-                + '&message=' + message
-            );
-        }
-    }
-
-    function handleReponse(form, request) {
-        var errorMessage = form.querySelector('.contact__form__message--error');
-        var successMessage = form.querySelector(
-            '.contact__form__message--success'
+        var name = form.querySelector('input[name="fname"]').value;
+        var email = form.querySelector('input[name="email"]').value;
+        var phone = form.querySelector('input[name="phone"]').value;
+        var company = form.querySelector('input[name="company"]').value;
+        var message = form.querySelector('textarea').value;
+        message =  encodeURIComponent(
+            'Organisation: ' + company
+            + '\n'
+            + '\n'
+            + message
         );
 
-        if(request.readyState === XMLHttpRequest.DONE) {
-
-            console.log(request.responseText);
-            if (request.status === 200) {
-                successMessage.removeAttribute('hidden');
-            } else {
-                errorMessage.removeAttribute('hidden');
-            }
-        }
+        return (
+            'name=' + name
+            + '&email=' + email
+            + '&phone=' + phone
+            + '&message=' + message
+        );
     }
 
     function submitForm(form) {
@@ -50,12 +30,30 @@
             var url = 'http://getsimpleform.com/messages/ajax?form_api_token=e81d577ce1527ee5f3cd7ecc6208826f';
 
             var params = getParams(form);
+            var errorMessage = form.querySelector(
+                '.contact__form__message--error'
+            );
+            var successMessage = form.querySelector(
+                '.contact__form__message--success'
+            );
 
             request.open("POST", url, true);
             request.setRequestHeader(
                 "Content-type", "application/x-www-form-urlencoded"
             );
-            request.onreadystatechange = handleReponse(form, request);
+
+            request.onreadystatechange = function() {
+                if(request.readyState === 4) {
+
+                    if (request.status === 200) {
+                        successMessage.removeAttribute('hidden');
+                        form.reset();
+                    } else {
+                        errorMessage.removeAttribute('hidden');
+                    }
+                }
+            };
+
             request.send(params);
         }
     }
@@ -73,12 +71,19 @@
         }, false);
     }
 
+    function initForm(form) {
+        if (form) {
+            form.addEventListener('submit', function (e) {
+                e.preventDefault();
+                submitForm(form);
+            });
+        }
+    }
+
     document.addEventListener('DOMContentLoaded', function() {
         var form = document.querySelector('#contact-form');
+
+        initForm(form);
         initMobileMenu();
-
-        if (form) {
-
-        }
     }, false);
 } ());
